53f3c9d0943cef77e42dc72ae7cbbee52bd4f6b3
samPuriwat <sam.puriwat@gmail.com>
2018-02-23 11:53:06 +0700

    Show Dashboard

--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -3,6 +3,8 @@ package sample;
 import dbUtil.dbConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
@@ -23,4 +25,30 @@ public class loginModel {
     public boolean isDatabaseConnection(){
         return this.connection != null;
     }//isDatabaseConnection
+
+    public boolean isLogin(String user, String pass) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from user where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,user);
+            pr.setString(2, pass);
+            rs = pr.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            pr.close();
+            rs.close();
+        }
+
+
+    }//isLogin
 }//class
