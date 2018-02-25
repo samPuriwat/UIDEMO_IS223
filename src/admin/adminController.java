9ecdce727a75b58f9291696c7a3c2af074aa5a56
samPuriwat <sam.puriwat@gmail.com>
2018-02-25 17:14:04 +0700

    Save Student Data To Database

--- src/admin/adminController.java
+++ src/admin/adminController.java
@@ -212,6 +212,35 @@ public class adminController implements Initializable {
 
     }//editStudent
 
+    @FXML
+    private void saveStudent(ActionEvent event){
+        StudentData std = studentTable.getSelectionModel().getSelectedItem();
+        //DialogMessage
+        JOptionPane.showConfirmDialog(null,
+                "Do you want to update student ID: "
+                        + std.getId());
+        String sqlUpdate = "update student set firstName = ?," +
+                "lastName = ?,email = ?, DOB = ? where ID = ?";
+        try {
+            Connection conn = dbConnection.getConnection();
+            PreparedStatement pr = conn.prepareStatement(sqlUpdate);
+            pr.setString(1,this.txtFirstName.getText());
+            pr.setString(2,this.txtLastName.getText());
+            pr.setString(3,this.txtEmail.getText());
+            pr.setString(4, this.txtDOB.getEditor().getText());
+            pr.setString(5,this.txtID.getText());
+
+            pr.executeUpdate();
+            pr.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        loadStudentData(new ActionEvent());
+
+
+    }//saveStudent
+
 
 
 
