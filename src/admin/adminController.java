58cc504012959a09b72a124dd29d6ae6cc8d3e64
samPuriwat <sam.puriwat@gmail.com>
2018-02-25 17:04:15 +0700

    Edit Student Data from TableView

--- src/admin/adminController.java
+++ src/admin/adminController.java
@@ -195,6 +195,22 @@ public class adminController implements Initializable {
         loadStudentData(new ActionEvent());
 
     }//deleteStudent
+    @FXML
+    private void editStudent(ActionEvent event){
+        StudentData std = studentTable.getSelectionModel().getSelectedItem();
+        if (std != null) {
+            txtID.setText(std.getId());
+            //disable txtfiled to read only
+            txtID.setDisable(true);
+            txtFirstName.setText(std.getFirstName());
+            txtLastName.setText(std.getLastName());
+            txtEmail.setText(std.getEmail());
+            txtDOB.getEditor().setText(std.getDOB());
+        } else {
+            System.exit(1);
+        }
+
+    }//editStudent
 
 
 
